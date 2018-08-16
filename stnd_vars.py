c910ef543a1cf9a478b2e2a54d8f98751904e732
admin <easyfastexcel@gmail.com>
2018-08-16 08:38:24 -0700

    Revert "adding .gitignore"
    This reverts commit 97c0bb99fe89c5893cc7ae223f0013084e7ff640.

--- stnd_vars.py
+++ stnd_vars.py
@@ -1,7 +1,6 @@
 class StandardMessages:
     msg_contactak = 'Contact Alfonso Kondo (alfonso.kondo@gmail.com) for technical support'
     msg_timedout = "[DOWNLOAD ERROR] Process timed out while attempting download of {}"
-    msg_testmsg = "this is a test messagge"
 
 #
 # print(StandardMessages.msg_contactak)
